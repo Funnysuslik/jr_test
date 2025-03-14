81ace021b1f14a37c8f3fae1270240190127811e
Aleksandr Stepanov <stepanov.step@gmail.com>
2025-03-14 18:20:26 +0400

    feature/Add controllers

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,42 @@
+class Api::V1::PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    limit = (params[:n] || 10).to_i
+    posts = Post.left_joins(:ratings)
+               .select('posts.*, COALESCE(AVG(ratings.value), 0) as avg_rating')
+               .group('posts.id')
+               .order('avg_rating DESC')
+               .limit(limit)
+
+    render json: posts.as_json(only: [:id, :title, :body])
+  end
+
+  def create
+    user = User.find_or_initialize_by(login: params[:user_login])
+    user.save! if user.new_record?
+
+    post = Post.new(
+      title: params[:title],
+      body: params[:body],
+      user: user,
+      ip: params[:ip], # request.remote_ip
+    )
+
+    if post.save
+      render json: post.as_json(include: :user), status: :created
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def ip_list
+    results = Post.joins(:user)
+                  .group(:ip)
+                  .select('ip, array_agg(distinct users.login) as authors')
+                  .having('count(distinct user_id) > 1')
+                  .map { |p| { ip: p.ip, authors: p.authors } }
+
+    render json: results
+  end
+end
---
+++ app/controllers/api/v1/ratings_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::RatingsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    post = Post.find(params[:id])
+    user = User.find(params[:user_id])
+
+    rating = post.ratings.new(user: user, value: params[:value])
+
+    if rating.save
+      average = post.ratings.average(:value).to_f.round(2)
+      render json: { average_rating: average }
+    else
+      render json: { errors: rating.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Post/User not found" }, status: :not_found
+  rescue ActiveRecord::RecordNotUnique
+    render json: { error: "You've already rated this post" }, status: :conflict
+  end
+end
