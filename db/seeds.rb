0af07f5020b7a8f4baa50448ecb643e7c83da31b
Aleksandr Stepanov <stepanov.step@gmail.com>
2025-03-15 18:33:03 +0400

    feature/Add functionality in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'curb'
+require 'json'
+require 'securerandom'
+
+curl = Curl::Easy.new
+curl.headers['Content-Type'] = 'application/json'
+
+200_000.times do |i|
+  body = {
+    user_login: "user_#{i < 100 ? i : rand(100)}",
+    title: "Post Title #{i}",
+    body: "This is the content for post #{i}.",
+    ip: "192.168.1.#{rand(50)}",
+  }
+
+  curl.url = 'http://localhost:3000/api/v1/posts'
+  curl.post_body = body.to_json
+  curl.perform
+
+  if i >= 100 && rand < 0.75
+    rand(1..5).times do
+      rating_body = {
+        user_id: rand(100),
+        post_id: i + 1,
+        value: rand(1..5),
+      }
+
+      curl.url = 'http://localhost:3000/api/v1/ratings'
+      curl.post_body = rating_body.to_json
+      curl.perform
+    end
+  end
+end
